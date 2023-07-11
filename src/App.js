48e2adabdb498670c8a9754bd5a804cc54d9c87f
Nusry <139093688+Nuscross@users.noreply.github.com>
2023-07-11 07:13:13 +0000

    Update 11 files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
@@ -6,50 +6,31 @@ function App() {
       id: "e1",
       title: "Toilet Paper",
       amount: "294.67",
-      date: new Date(2023, 1, 5)
+      date: new Date(2023, 1, 5),
     },
     {
       id: "e2",
       title: "Car Insurance",
       amount: "304.67",
-      date: new Date(2023, 2, 10)
+      date: new Date(2023, 2, 10),
     },
     {
       id: "e3",
       title: "New TV",
       amount: "314.67",
-      date: new Date(2023, 3, 15)
+      date: new Date(2023, 3, 15),
     },
     {
       id: "e4",
       title: "New Desk",
       amount: "324.67",
-      date: new Date(2023, 4, 20)
-    }
+      date: new Date(2023, 4, 20),
+    },
   ];
 
   return (
     <div>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
 }
